81fd0be553c70b49ebc3d578aecaf3957c18ae5e
sadiqbehzad <sadiq.alibehzad@gmail.com>
2023-10-26 23:26:09 -0600

    261023 23:26

---
+++ test2.js
@@ -0,0 +1,8 @@
+function changePic(id, att, path) {
+  let obj = document.getElementById(id);
+  obj.setAttribute(att, path);
+}
+function restorePic(id, att, path) {
+  let obj = document.getElementById(id);
+  obj.setAttribute(att, path);
+}
