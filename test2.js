7d8a4b23ca9365630a06be01c8ddd528c0ea4b56
sadiqbehzad <sadiq.alibehzad@gmail.com>
2023-10-28 00:06:23 -0600

    281023 00:06

--- test2.js
+++ test2.js
@@ -7,15 +7,24 @@ function restorePic(id, att, path) {
   obj.setAttribute(att, path);
 }
 
-// function showDropdown(id) {
-//   document.getElementById(id).style.cssText = "display: grid";
-// }
-// function hideDropdown(id) {
-//   document.getElementById(id).style.cssText = "display: none";
-// }
+function showHideMenu(id) {
+  if (document.getElementById(id).style.display === "grid") {
+    hideMenu(id);
+  } else {
+    showMenu(id);
+  }
+}
+
+function showMenu(id) {
+  document.getElementById(id).style.cssText = "display: grid";
+}
+
+function hideMenu(id) {
+  document.getElementById(id).style.cssText = "display: none";
+}
 function basta() {
   console.log("Hi, this is CLOSE function");
-  document.getElementById("dropdownmenu").style.cssText = "display:none";
+  document.getElementById("navigate").style.cssText = "display:none";
   let dropdownm = document.getElementById("dropdownmenu");
   dropdownm.removeChild(dropdownm.firstElementChild);
 }
