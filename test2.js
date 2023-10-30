3d1e36838fe85bf6cae2c4cd868616c668a33689
sadiqbehzad <sadiq.alibehzad@gmail.com>
2023-10-29 23:24:42 -0600

    291023 23:24

--- test2.js
+++ test2.js
@@ -27,3 +27,23 @@ window.addEventListener("resize", function () {
   "use strict";
   window.location.reload();
 });
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "anootech23@gmail.com",
+    Password: "58A11570B3D74B8CE0FC0CB6DA22BDF962A6",
+    To: "anootech23@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "Random Subject (Must be edited in the form",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Phone number: " +
+      document.getElementById("phone").value +
+      "<br> message: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Message sent successfully"));
+}
